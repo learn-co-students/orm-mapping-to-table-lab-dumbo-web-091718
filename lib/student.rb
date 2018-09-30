2f5f934bc295481e0c2057b5d346b8e685e310a8
github username <github email address>
2018-09-30 00:40:08 -0400

    Mapped to Database Ruby-SQL

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,71 @@
+#DB[:conn].execute("do something to database...")
 class Student
 
+  attr_accessor :name, :grade
+  attr_reader :id
+
+
+
+  def initialize(name, grade, id=nil)
+        @id = id
+        @name = name
+        @grade = grade
+
+          # attributes = { @name => name, @grade => grade }
+          #
+          # attributes.each do |key, value|
+
+          # if self.respond_to?("#{key}=")
+          #    self.send("#{key}=", value)
+          #  end
+
+          # end
+
+
+  end
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+  # *** CREATE TABLE ***
+    def self.create_table
+       sql = <<-SQL
+
+         CREATE TABLE students (
+           id INTEGER PRIMARY KEY,
+           name TEXT,
+           grade TEXT
+         )
+       SQL
+
+       DB[:conn].execute(sql)
+     end
+
+
+    # **** DROP TABLE ***
+    def self.drop_table
+       sql = <<-SQL
+       DROP TABLE students;
+       SQL
+
+       DB[:conn].execute(sql)
+       # one line drop
+       # DB[:conn].execute("DROP TABLE students;")
+    end
+
+    # **** SAVE ***
+    def save
+      sql = <<-SQL
+        INSERT INTO students(name, grade)
+        VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students;")[0][0]
+    end
+
+     # **** CREATE  ***
+    def self.create(name:, grade:)
+      new_student = self.new(name, grade)
+      new_student.save
+      new_student
+    end
+
 end
